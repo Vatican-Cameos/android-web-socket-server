7fae003a7aa4385d9515e7f87bf63af09ec877aa
Pavan <pavanvenkatesh94@gmail.com>
2017-03-01 16:24:27 +0530

    :sparkles: initial commit

---
+++ app/src/main/java/in/goodiebag/android_web_socket_server/MainActivity.java
@@ -0,0 +1,13 @@
+package in.goodiebag.android_web_socket_server;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
